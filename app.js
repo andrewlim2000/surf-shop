88e5962c9e02d54f4c88f1cd28695a26c65f12d7
Andrew Lim <2000andrewlim@gmail.com>
2020-05-27 17:56:24 -0700

    make bug fixes and updates

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
 const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
