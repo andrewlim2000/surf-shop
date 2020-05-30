41aebc397cf1ac1ebe5050ace5fd3e19c537981e
Andrew Lim <2000andrewlim@gmail.com>
2020-05-29 18:39:54 -0700

    update user (index) routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 const passport = require('passport');
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const util = require('util');
-const { clodinary } = require('../cloudinary');
+const { cloudinary } = require('../cloudinary');
 const { deleteProfileImage } = require('../middleware');
 
 module.exports = {
@@ -77,7 +77,7 @@ module.exports = {
     if (username) user.username = username;
     if (email) user.email = email;
     if (req.file) {
-      if (user.image.public_id) await clodinary.v2.uploader.destroy(user.image.public_id);
+      if (user.image.public_id) await cloudinary.v2.uploader.destroy(user.image.public_id);
       const { secure_url, public_id } = req.file;
       user.image = { secure_url, public_id };
     }
--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
   image: {
-    secure_url: { type: String, default: '/images/default-profile.img' },
+    secure_url: { type: String, default: '/images/default-profile.jpg' },
     public_id: String
   }
 });
